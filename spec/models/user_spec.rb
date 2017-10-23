548bf4056791818ea12be9f793bab661f8963138
RubyRed76 <chris.moncure.aws@gmail.com>
2017-10-22 21:16:56 -0400

    First user_spec.rb Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   #pending "add some examples to (or delete) #{__FILE__}"
+# end
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "creation" do
+		it "can be created" do
+			user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+			expect(user).to be_valid
+		end
+	end
+
+############## LOOKS LIKE SAME THING TO ME BUT NOT WORKING!!! ##################################
+# RSpec.describe User, type: :model do
+#   describe “creation” do
+# 		it “can be created” do
+# 			user = User.create(email: “test@test.com”,  password: “asdfasdf”, password_confirmation: “asdfasdf”)
+# 			expect(user).to be_valid
+# 		end
+#   end
+
+
+
+	#When you use “describe” in rspec, it is essentially a way of organizing your tests.  
+	#I’m going to first make sure that a user can be created:
+
 end
+
+
+
+
